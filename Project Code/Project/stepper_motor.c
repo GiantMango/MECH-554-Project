599b54e9c203cbddece9f63f5ff67e58f41481d4
Taylor <subway1101@gmail.com>
2023-11-15 13:41:49 -0800

    init stepper motor done

--- Project Code/Project/stepper_motor.c
+++ Project Code/Project/stepper_motor.c
@@ -5,67 +5,61 @@
 #include "sensor.h"
 #include "lcd.h"
 
+/* GLOBAL VARIABLES */
+volatile unsigned char current_step;
+volatile unsigned char steps[4] = {STEP1, STEP2, STEP3, STEP4};
+volatile unsigned char half_steps[8] = {STEP05, STEP1, STEP15, STEP2, STEP25, STEP3, STEP35, STEP4};
+
 /* Initialize Stepper Motor to Black */
 void init_stepper_motor(){
-	while(HE){
-		// TODO: ADD initialize stepper motor rotating function with half step commands
-		StepperMotor_CW(1);
+
+	DDRA |= (_BV(PA0) | _BV(PA1) | _BV(PA2) | _BV(PA3) | _BV(PA4) | _BV(PA5));
+
+	int temp = 0;
+
+	while(!HE){ // HE: active low
+		STEPPER_MOTOR_PORT = (STEPPER_MOTOR_PORT & ~STEPPER_MOTOR_MASK) | steps[temp % 4];
+		temp += 1;
 	}
+
+	current_step = INIT_STEP;
+}
+
+
+void StepperMotor_CW (int num_steps){
+    while(num_steps > 0){
+        STEPPER_MOTOR_PORT = (STEPPER_MOTOR_PORT & ~STEPPER_MOTOR_MASK) | steps[num_steps%4];
+        num_steps -= 1;
+    }
 }
 
 // TODO: Optimize StepperMotor_CW and CCW
-void StepperMotor_CCW(int num){
-	for (int i = num; i > 0; i--){
-		switch (current_step)
-		{
-			case(1):
-				PORTA = STEP2;
-				current_step = 2;
-				mTimer(20);
-				break;
-			case(2):
-				PORTA = STEP3;
-				current_step = 3;
-				mTimer(20);
-				break;
-			case(3):
-				PORTA = STEP4;
-				current_step = 4;
-				mTimer(20);
-				break;
-			case(4):
-				PORTA = STEP1;
-				current_step = 1;
-				mTimer(20);
-				break;
-		}
-	}
-}
-
-void StepperMotor_CW(int num_steps){
+void StepperMotor_CCW(int num_steps){
 	for (int i = num_steps; i > 0; i--){
 		switch (current_step)
 		{
 			case(1):
-				PORTA = STEP4;
-				current_step = 4;
-				mTimer(20);
-				break;
-			case(2):
-				PORTA = STEP1;
-				current_step = 1;
-				mTimer(20);
-				break;
-			case(3):
 				PORTA = STEP2;
 				current_step = 2;
 				mTimer(20);
 				break;
-			case(4):
+			case(2):
 				PORTA = STEP3;
 				current_step = 3;
+				mTimer(20);
+				break;
+			case(3):
+				PORTA = STEP4;
+				current_step = 4;
+				mTimer(20);
+				break;
+			case(4):
+				PORTA = STEP1;
+				current_step = 1;
 				mTimer(20);
 				break;
 		}
 	}
 }
+
+
--- Project Code/Project/stepper_motor.h
+++ Project Code/Project/stepper_motor.h
@@ -1,35 +1,24 @@
 ﻿#ifndef STEPPER_MOTOR_H_
 #define STEPPER_MOTOR_H_
 
-#define STEPPER_MOTOR_PORT		PORT(C)
-
-#define EN1			              PA5
-#define L1			              PA4
-#define L2			              PA3
-#define EN2			              PA2
-#define L3			              PA1
-#define L4			              PA0   
+#define STEPPER_MOTOR_PORT		PORT(A)
+#define STEPPER_MOTOR_MASK    0x3F
 
 // |    |    | EN1 | L1 | L2 | EN2 | L3 | L4 |
-#define STEP1                 (STEPPER_MOTOR_PORT = (1<<EN1 | 1<<L1))
-#define STEP2                 (STEPPER_MOTOR_PORT = (1<<EN2 | 1<<L3))
-#define STEP3                 (STEPPER_MOTOR_PORT = (1<<EN1 | 1<<L2))
-#define STEP4                 (STEPPER_MOTOR_PORT = (1<<EN2 | 1<<L4))
+#define STEP1                 0b00110000
+#define STEP2                 0b00000110
+#define STEP3                 0b00101000
+#define STEP4                 0b00000101
 
-#define STEP05                (STEPPER_MOTOR_PORT = (1<<EN1 | 1<<L1 | 1<<EN2 | 1<<L4))
-#define STEP15                (STEPPER_MOTOR_PORT = (1<<EN1 | 1<<L1 | 1<<EN2 | 1<<L3))
-#define STEP25                (STEPPER_MOTOR_PORT = (1<<EN1 | 1<<L2 | 1<<EN2 | 1<<L3))
-#define STEP35                (STEPPER_MOTOR_PORT = (1<<EN1 | 1<<L2 | 1<<EN2 | 1<<L4))
+#define STEP05                0b00110101
+#define STEP15                0b00110110
+#define STEP25                0b00101110
+#define STEP35                0b00101101
 
-#define STEPS                 (STEP1, STEP2, STEP3, STEP4)
-#define HALF_STEPS            (STEP05, STEP1, STEP15, STEP2, STEP25, STEP3, STEP35, STEP4)
 #define INIT_STEP             2
 
 #define DEG90                 50
 #define DEG180                100
-
-/* GLOBAL VARIABLES */
-volatile int current_step;
 
 void init_stepper_motor();
 void StepperMotor_CW(int num_steps);
