d13ec8fdf2ee0f670b8edd5709949345e7413f92
Taylor <subway1101@gmail.com>
2023-11-24 09:10:40 -0800

    [fix] Correct stepper motor table

--- Project Code/Project/stepper_motor.c
+++ Project Code/Project/stepper_motor.c
@@ -8,9 +8,13 @@
 /* GLOBAL VARIABLES */
 volatile unsigned char current_step;
 volatile unsigned char steps[4] = {STEP1, STEP2, STEP3, STEP4};
-volatile unsigned char half_steps[8] = {STEP05, STEP1, STEP15, STEP2, STEP25, STEP3, STEP35, STEP4};
+//volatile unsigned char half_steps[8] = {STEP05, STEP1, STEP15, STEP2, STEP25, STEP3, STEP35, STEP4};
 
-
+volatile unsigned int accel_speed[ACCEL_TOTAL_STEPS] = {25000, 20000, 16667, 14286, 12500,
+11111, 10000, 9091, 8333, 7692,
+7143, 6667, 6250, 5882, 5556,
+5263, 5000, 4762, 4545, 4348};
+// 4167, 4000, 3846, 3704, 3571};
 
 
 /* Initialize Stepper Motor to Black */
@@ -40,14 +44,35 @@ void StepperMotor_Rotate(int num_steps){
 }
 
 
-void StepperMotor_CW(int num_steps){
+void StepperMotor_CW(int num_steps){ // 12  24  60
     int i = 0;
     while(i < num_steps){
-        STEPPER_MOTOR_PORT = (STEPPER_MOTOR_PORT & ~STEPPER_MOTOR_MASK) | steps[(current_step+i)%4];
-        i += 1;
-				mTimer(20);
+				STEPPER_MOTOR_PORT = (STEPPER_MOTOR_PORT & ~STEPPER_MOTOR_MASK) | steps[(current_step+i)%4];
+				// uTimer(20000);
+
+				/* Acceleration and Deceleration Profile */
+				if(num_steps <= 2*ACCEL_TOTAL_STEPS){
+						if(i <= num_steps/2){ 
+							uTimer(accel_speed[i]);
+						} else if (i > num_steps/2 ) {
+							uTimer(accel_speed[num_steps - i - 1]);
+						}
+				} else {
+						if(i < ACCEL_TOTAL_STEPS){ 
+							uTimer(accel_speed[i]);
+						} else if (i > (num_steps - ACCEL_TOTAL_STEPS - 1)) {
+							uTimer(accel_speed[num_steps - i - 1]);
+						} else {
+							uTimer(accel_speed[ACCEL_TOTAL_STEPS - 1]);
+						}
+				}
+				/* End of Accel/Deccel Profile */
+				
+				i += 1;
     }
+		
 		current_step = (current_step+i)%4;
+
 }
 
 
@@ -55,13 +80,30 @@ void StepperMotor_CCW(int num_steps){
     int i = 0;
     while(i < num_steps){
         STEPPER_MOTOR_PORT = (STEPPER_MOTOR_PORT & ~STEPPER_MOTOR_MASK) | steps[3 - (current_step+i)%4];
-        i += 1;
-				mTimer(20);
+				// uTimer(20000);
+
+				/* Acceleration and Deceleration Profile */
+				if(num_steps <= 2*ACCEL_TOTAL_STEPS){
+						if(i <= num_steps/2){ 
+							uTimer(accel_speed[i]);
+						} else if (i > num_steps/2 ) {
+							uTimer(accel_speed[num_steps - i - 1]);
+						}
+				} else {
+						if(i < ACCEL_TOTAL_STEPS){ 
+							uTimer(accel_speed[i]);
+						} else if (i > (num_steps - ACCEL_TOTAL_STEPS-1)) {
+							uTimer(accel_speed[num_steps - i - 1]);
+						} else {
+							uTimer(accel_speed[ACCEL_TOTAL_STEPS-1]);
+						}
+				}
+				/* End of Accel/Deccel Profile */
+
+				i += 1;
 		}
 		current_step = 3 - (current_step+i)%4;
 }
-
-// TODO: Accel and deccel
 
 
 
--- Project Code/Project/stepper_motor.h
+++ Project Code/Project/stepper_motor.h
@@ -3,17 +3,17 @@
 
 #define STEPPER_MOTOR_PORT		PORT(A)
 #define STEPPER_MOTOR_MASK    0x3F
-
+// |Bit7|Bit6|Bit5 |Bit4|Bit3 |Bit2|Bit1|Bit0|
 // |    |    | EN1 | L1 | L2 | EN2 | L3 | L4 |
-#define STEP1                 0b00110000
-#define STEP2                 0b00000110
-#define STEP3                 0b00101000
-#define STEP4                 0b00000101
+#define STEP1                 0b00110101
+#define STEP2                 0b00110110
+#define STEP3                 0b00101110
+#define STEP4                 0b00110101
 
-#define STEP05                0b00110101
-#define STEP15                0b00110110
-#define STEP25                0b00101110
-#define STEP35                0b00101101
+// #define STEP05                0b00110101
+// #define STEP15                0b00110110
+// #define STEP25                0b00101110
+// #define STEP35                0b00101101
 
 #define INIT_STEP             2
 
@@ -21,11 +21,15 @@
 #define DEG90                 50
 #define DEG180                100
 
-#define MAX_SPEED             3300  // pulse per second (pps) (Hz)
-#define ACCEL
-#define DECCEL
+#define MAX_SPEED             3000  // pulse per second (pps) (Hz) @ max speed 300us per step
+#define ACCEL_TOTAL_STEPS     20
 
-volatile unsigned char plate_ready_flag;
+
+// volatile unsigned int deccel_speed[ACCEL_TOTAL_STEPS] = {333, 348, 364, 381, 400, 421,
+//                                                          444, 471, 500, 533, 571, 615,
+//                                                          667, 727, 800, 889, 1000, 1143,
+//                                                          1333, 1600, 2000, 2667, 4000, 8000};
+
 
 void init_stepper_motor();
 void StepperMotor_Rotate(int num_steps);
