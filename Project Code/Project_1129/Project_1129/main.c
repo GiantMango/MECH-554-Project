0a1d2fc9e104fc43112486a86d47ec717f86d27f
Taylor <subway1101@gmail.com>
2023-11-29 16:26:53 -0800

    cleared everything

---
+++ Project Code/Project_1129/Project_1129/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/29/2023 4:25:50 PM
+ *  Author: subwa
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
